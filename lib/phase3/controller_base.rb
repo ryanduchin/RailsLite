2c1f95d889429e3f19e7f7d494348275dc55c6de
Ryan Duchin <ryanduchin@gmail.com>
2015-05-19 15:52:48 -0700

    phase 3 completed. controller has render(template_name) method, which converts html.erb template to an ERB file, evaulate and binds it, then uses phase 2 render_content method to display in page

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -2,12 +2,25 @@ require_relative '../phase2/controller_base'
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      controller_name = self.class.to_s.underscore
+        #benefit of having this in a module!! :)
+
+      string = File.read("./views/#{controller_name}/#{template_name}.html.erb")
+        #"./" for project root dir
+
+      template = ERB.new(string)
+      capture = template.result(binding) #evaluate and bind
+
+      render_content(capture, "text/html")
+
+
     end
   end
 end
